4585407630354656f99fcd079be9ef8085c7e055
art102 <a5903281@ya.ru>
2017-10-06 16:36:07 +0300

    prod

--- src/main/java/rs5/persistence/service/RoleService.java
+++ src/main/java/rs5/persistence/service/RoleService.java
@@ -9,43 +9,43 @@ import rs5.persistence.repository.RoleRepository;
 
 import java.util.List;
 
-@Service
-public class RoleService {
+    @Service
+    public class RoleService {
 
-    @Autowired
-    RoleRepository roleRepository;
+        @Autowired
+        RoleRepository roleRepository;
 
-    @Transactional
-    public Role getRole(Long id){
-        return roleRepository.findOne(id);
-    }
+        @Transactional
+        public Role getRole(Long id){
+            return roleRepository.findOne(id);
+        }
 
-    @Transactional
-    public List<Role> getAll(){
-        return roleRepository.findAll();
-    }
+        @Transactional
+        public List<Role> getAll(){
+            return roleRepository.findAll();
+        }
 
-    @Transactional
-    public void addRole(Role role){
-        roleRepository.saveAndFlush(role);
-    }
+        @Transactional
+        public void addRole(Role role){
+            roleRepository.saveAndFlush(role);
+        }
 
-    @Transactional
-    public void updateRole(Role role){
-        roleRepository.saveAndFlush(role);
-    }
+        @Transactional
+        public void updateRole(Role role){
+            roleRepository.saveAndFlush(role);
+        }
 
-    @Transactional
-    public void deleteRole(Role role){
-        roleRepository.delete(role);
-    }
+        @Transactional
+        public void deleteRole(Role role){
+            roleRepository.delete(role);
+        }
 
-    @Transactional
-    public void deleteRole(Long id){
-        roleRepository.delete(id);
+        @Transactional
+        public void deleteRole(Long id){
+            roleRepository.delete(id);
+        }
+        @Transactional
+        public Boolean roleExists(Long id){
+            return roleRepository.exists(id);
+        }
     }
-    @Transactional
-    public Boolean roleExists(Long id){
-        return roleRepository.exists(id);
-    }
-}
